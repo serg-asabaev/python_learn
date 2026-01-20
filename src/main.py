37ae194fd858d76779d1de27007eeab17201fccb
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-01-20 21:52:36 +0400

    added funcion add()

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from typing import Union
 # import requests
 
 import masks
+import widget
 
 
 def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
@@ -19,3 +20,8 @@ def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
 print(masks.get_mask_card_number(''))
 
 print(masks.get_mask_account('73654108430135874305'))
+
+print(widget.mask_account_card("Счет 73654108430135874305"))
+print(widget.mask_account_card("Visa Platinum 7000792289606361"))
+
+print(widget.get_date("2024-03-11T02:26:18.671407"))
