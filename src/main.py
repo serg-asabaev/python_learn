8ce7ab955debcc77b0a10148343561e2ef75fd49
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-01-20 22:58:14 +0400

    changes in main.py

--- src/main.py
+++ src/main.py
@@ -17,12 +17,14 @@ def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
 # print(add(5, 8))
 # print(math.cos(2))
 
-print(masks.get_mask_card_number(''))
+if __name__ == '__main__':
 
-print(masks.get_mask_account('73654108430135874305'))
+    print(masks.get_mask_card_number(''))
 
-print(widget.mask_account_card("Счет 73654108430135874305"))
-print(widget.mask_account_card("Visa Platinum 7000792289606361"))
+    print(masks.get_mask_account('73654108430135874305'))
 
-print(widget.get_date("2024-03-11T02:26:18.671407"))
-#test comment
+    print(widget.mask_account_card("Счет 73654108430135874305"))
+    print(widget.mask_account_card("Visa Platinum 7000792289606361"))
+
+    print(widget.get_date("2024-03-11T02:26:18.671407"))
+    #test comment
