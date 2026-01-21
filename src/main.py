f65bb5a2d3ffef374dcf27852fc0f74e63298a6f
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-01-21 22:38:52 +0400

    add functions to documentation

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
 
 if __name__ == '__main__':
 
-    print(masks.get_mask_card_number(''))
+    print(masks.get_mask_card_number('7000792289606361'))
 
     print(masks.get_mask_account('73654108430135874305'))
 
